a12f427fc20eddd8c1f157af8ab8495b333c2f6c
grantorin <red.chekist@gmail.com>
2019-04-27 19:01:11 +0300

    Update chartist-plugin-targetline.js
    Added optional direction along the x or y axis, as well as the ability to set the line in extreme positions.

--- chartist-plugin-targetline.js
+++ chartist-plugin-targetline.js
@@ -5,34 +5,59 @@
  * Based on https://github.com/gionkunz/chartist-plugin-pointlabels
  */
 /* global Chartist */
-(function(window, document, Chartist) {
-  'use strict';
+( function( window, document, Chartist ) {
+    'use strict';
 
-  var defaultOptions = {
-    class: 'ct-target-line',
-    value: null
-  };
-
-  Chartist.plugins = Chartist.plugins || {};
-  Chartist.plugins.ctTargetLine = function(options) {
-
-    options = Chartist.extend({}, defaultOptions, options);
-
-    return function ctTargetLine(chart) {
-        function projectY(chartRect, bounds, value) {
-					return chartRect.y1 - (chartRect.height() / bounds.max * value)
-				}
-
-        chart.on('created', function (context) {
-          var targetLineY = projectY(context.chartRect, context.bounds, options.value);
-
-          context.svg.elem('line', {
-            x1: context.chartRect.x1,
-            x2: context.chartRect.x2,
-            y1: targetLineY,
-            y2: targetLineY
-          }, options.class);
-        });
+    var defaultOptions = {
+        class: 'ct-target-line',
+        value: null,
+        axis: 'y'
     };
-  };
-}(window, document, Chartist));
+
+    Chartist.plugins = Chartist.plugins || {};
+    Chartist.plugins.ctTargetLine = function( options ) {
+
+        options = Chartist.extend( {}, defaultOptions, options );
+
+        return function ctTargetLine( chart ) {
+            function projectY( chartRect, bounds, value ) {
+                if (value === -1) {
+                    return chartRect.y1 + chartRect.height();
+                }
+                return chartRect.y1 - ( chartRect.height() / bounds.max * value )
+            }
+
+            function projectX( chartRect, bounds, value ) {
+                if (value === -1) {
+                    return chartRect.x1 + chartRect.width();
+                }
+                return chartRect.x1 + ( chartRect.width() / bounds.max * value )
+            }
+
+
+            chart.on( 'created', function( context ) {
+                if ( options.axis === "y" ) {
+                    var targetLineY = projectY( context.chartRect, context.bounds, options.value );
+
+                    context.svg.elem( 'line', {
+                        x1: context.chartRect.x1,
+                        x2: context.chartRect.x2,
+                        y1: targetLineY,
+                        y2: targetLineY
+                    }, options.class );
+
+                } else {
+                    var targetLineX = projectX( context.chartRect, context.bounds, options.value );
+
+                    context.svg.elem( 'line', {
+                        x1: targetLineX,
+                        x2: targetLineX,
+                        y1: context.chartRect.y1,
+                        y2: context.chartRect.y2
+                    }, options.class );
+                }
+
+            } );
+        };
+    };
+}( window, document, Chartist ) );
